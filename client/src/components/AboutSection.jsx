bf7c7eb20f0ee8b941076ba7ea7bb0a5d554cd15
VIRUS-VS <vishwachiniwar@gmail.com>
2025-09-11 15:08:09 +0530

    small changes

--- client/src/components/AboutSection.jsx
+++ client/src/components/AboutSection.jsx
@@ -7,7 +7,7 @@ const AboutSection = () => {
     const aboutImage1 = settings?.aboutImage1 ? settings.aboutImage1 : "[https://images.unsplash.com/photo-1598043596283-8873a1139b4b?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D](https://images.unsplash.com/photo-1598043596283-8873a1139b4b?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)";
     const aboutImage2 = settings?.aboutImage2 ? settings.aboutImage2 : "[https://images.unsplash.com/photo-1616684534720-3398d523826a?q=80&w=2803&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D](https://images.unsplash.com/photo-1616684534720-3398d523826a?q=80&w=2803&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)";
   return (
-    <div className="py-24 bg-gray-900">
+    <div className="py-24 bg-[#3C1D1D]">
       <div className="container mx-auto px-4 grid md:grid-cols-2 gap-12 items-center">
         <div className="text-center md:text-left">
           <h2 className="text-4xl font-thin mb-4 tracking-wider">{settings?.aboutTitle || "A MODERN APPROACH"}</h2>
