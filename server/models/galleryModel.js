b7e91f449061fc7c7b46ab5de5b48e252f9c754e
VIRUS-VS <vishwachiniwar@gmail.com>
2025-09-11 16:25:11 +0530

    Gallery changes

--- server/models/galleryModel.js
+++ server/models/galleryModel.js
@@ -5,14 +5,17 @@
 
 
 // In backend/models/galleryModel.js
-import mongoose from 'mongoose';
+// Use require instead of import
+const mongoose = require('mongoose');
 
 const gallerySchema = mongoose.Schema({
   title: { type: String, required: true },
   coverImage: { type: String, required: true },
-  description: { type: String }, // <-- ADD THIS LINE
-  youtubeLink: { type: String }, // <-- ADD THIS LINE
+  description: { type: String },
+  youtubeLink: { type: String },
 }, { timestamps: true });
 
 const Gallery = mongoose.model('Gallery', gallerySchema);
-export default Gallery;
+
+// Use module.exports instead of export default
+module.exports = Gallery;
